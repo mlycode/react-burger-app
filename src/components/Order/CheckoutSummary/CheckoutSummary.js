6f00d54ec98301245d1568c6a885dbd411039975
mlycode <m.ly@live.co.uk>
2018-09-05 11:32:03 +0100

    add checkout summary component and routing package

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.css";
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Enjoy your Burger!</h1>
+            <div style={{width: "100%", margin: "auto"}}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button clicked btnType="Danger">Cancel</Button>
+            <Button clicked btnType="Success">Continue</Button>
+        </div>
+    );
+}
+
+export default checkoutSummary;
