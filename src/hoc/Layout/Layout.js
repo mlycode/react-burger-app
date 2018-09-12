5ad5bfd6562d430f74a4a75e1af0d63c19a5dca5
mlycode <m.ly@live.co.uk>
2018-09-12 18:47:42 +0100

    change navigation bar to show signup/signin or logout depending on user authentication

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import { connect } from "react-redux";
+
 import Aux from "../Auxiliary/Auxiliary";
 import classes from "./Layout.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
@@ -22,7 +24,9 @@ class Layout extends Component {
     render () {
         return (
             <Aux>
-                <Toolbar open={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    open={this.sideDrawerToggleHandler}/>
                 <SideDrawer
                 closed={this.sideDrawerClosedHandler}
                 open={this.state.showSideDrawer} />
@@ -34,4 +38,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
