a294fc836f89881735653172d9189d2a310bd719
mlycode <m.ly@live.co.uk>
2018-09-12 20:07:42 +0100

    add logout button with redirect to homepage

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/">Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-       {props.isAuthenticated ? <NavigationItem link="/auth">Logout</NavigationItem> : <NavigationItem link="/auth">Sign up/Sign in</NavigationItem>}  
+       {props.isAuthenticated ? <NavigationItem link="/logout">Logout</NavigationItem> : <NavigationItem link="/auth">Sign up/Sign in</NavigationItem>}  
     </ul>
 );
 
