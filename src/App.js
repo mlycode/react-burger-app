95800b1b1ad5cc39d86a50a243c38c465e3290b4
mlycode <m.ly@live.co.uk>
2018-09-16 23:23:27 +0100

    link userId with orders page to only show own user's orders

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/auth" component={Auth} />
-        <Route path="/" component={BurgerBuilder} />
+        <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     );
@@ -31,6 +31,7 @@ class App extends Component {
             <Route path="/checkout"  component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
             <Redirect to="/" />
         </Switch>
       );
